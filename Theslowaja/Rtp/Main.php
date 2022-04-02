d9c7dd0b7b81d981b20ea1cd5597ab71949af609
Theslowaja <67841659+Theslowaja@users.noreply.github.com>
2022-04-02 11:10:25 +0700

    Update Main.php

--- Theslowaja/Rtp/Main.php
+++ Theslowaja/Rtp/Main.php
@@ -11,21 +11,21 @@ use pocketmine\command\{
 use pockdtmine\plugin\PluginBase;
 use pocketmine\event\Listener;
 
-use Theslowaja\Rtp\RtpTask
-
-class Main extends PluginBase{
+class Main extends PluginBase implements Listener{
 	
 	public function onEnable():void{
             $this->getServer()->getPluginManager()->registerEvents($this, $this);
+            $this->saveDefaultConfig();
 	}
 	
 	public function onCommand(CommandSender $p, Command $cmd, string $label, array $args): bool {
 		switch($cmd->getName()){
 			case "rtp":
 			     if($p instanceof Player){
-			        $p->teleport(mt_rand(0, 99999), mt_rand(70, 120), mt_rand(0, 99999), $this->getConfig()->get("world"));
-			     } else {
+                                $this->getScheduler()->scheduleDelayedTask(new DelayTask($this, $p->getName()), 20 * $this->getConfig()->get("delay"));
+		             } else {
                                  $p->sendMessage("§cOnly Player Can Run This Command!");
+                             }
 			break;
 		}
                 return true;
